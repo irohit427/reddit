b4cb8e66ba6f003005ea19212fbf780722bf5e40
cb-rohit.r <rohit.r@chargebee.com>
2020-08-27 00:13:49 +0530

    MikroOrm setup complete

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,17 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import mikroConfig from './mikro-orm.config';
+import console from "console";
 
 const main = async () => {
     const orm = await MikroORM.init(mikroConfig);
-    const post = orm.em.create(Post, {title: 'First Post'});
+    await orm.getMigrator().up();
+    // Test code 
+    /* const post = orm.em.create(Post, {title: 'First Post'});
     await orm.em.persistAndFlush(post);
 
+    const posts = await orm.em.find(Post, {});
+    console.log(posts); */
 }
 
 main().catch((err) => {
